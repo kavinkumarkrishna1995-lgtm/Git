08ca0efcda5494c64565a36ae4c59f9edde6aa91
kavinkumarkrishna1995-lgtm <kavinracer3@gmail.com>
2026-08-18 14:06:28 +0530

    playwright folder uploaded in git hub

--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -4,18 +4,18 @@ test('DemoQA Radio Checkbox Button', async ({ page }) => {
 
     await page.goto('https://demoqa.com/radio-button');
 
-    await page.locator("//div[@id='app']/descendant::label[@for='yesRadio']").click();
+    await page.locator("//input[@id='yesRadio']").click();
 
-    await page.locator("//div[@id='app']/descendant::label[@for='impressiveRadio']").click();
+    // await page.locator("//div[@id='app']/descendant::label[@for='impressiveRadio']").click();
 
-    await page.locator("//div[@id='app']/descendant::label[@for='noRadio']").click();
+    // await page.locator("//div[@id='app']/descendant::label[@for='noRadio']").click();
 
-    await page.goto('https://demoqa.com/checkbox');
+    // await page.goto('https://demoqa.com/checkbox');
 
-    await page.locator("//div[@id='app']/descendant::label[@for='tree-node-home']").click();
+    // await page.locator("//div[@id='app']/descendant::label[@for='tree-node-home']").click();
 
-    await page.goto('https://demoqa.com/buttons');
+    // await page.goto('https://demoqa.com/buttons');
 
-    await page.locator("//div[@id='app']/descendant::button[text()='Click Me']").click();
+    // await page.locator("//div[@id='app']/descendant::button[text()='Click Me']").click();
 
 });
